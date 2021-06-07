96af719f7527d8cd5ecc28b2ad0bea432a5749d2
Lucas Tavano <lc_tavano@hotmail.com>
2021-06-07 00:57:33 -0300

    fixing project name

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SimpleDataAnalysis.MixProject do
 
   def project do
     [
-      app: :image_recognition_neural_networks,
+      app: :image_neural_network_labs,
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
